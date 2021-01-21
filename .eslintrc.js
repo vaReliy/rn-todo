ae736d824574657425078f27df4f7ac7bcce9148
Valeriy Garbuzov <valeriy@i.ua>
2021-01-21 10:56:48 +0200

    added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': [
+    'plugin:react/recommended',
+    'google',
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint',
+  ],
+  'rules': {
+    'arrow-parens': 'off',
+    'comma-dangle': 'warn',
+    'object-curly-spacing': 'off',
+    'require-jsdoc': 'off',
+    'semi': 'off',
+  },
+};
